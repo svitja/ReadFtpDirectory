9f42466895cea36b0bd4f15c4b6f93ae3614b907
Viktor.Sevch <viktor.sevch@transoftgroup.com>
2025-10-23 21:43:19 +0300

    1

---
+++ ReadFtpLog.cs
@@ -0,0 +1,23 @@
+﻿using Tsg.Sys;
+
+namespace ReadFtpDirectory
+{
+    public class ReadFtpLog : DefaultLog
+    {
+		protected override string LogFileName
+		{
+			get
+			{
+				return "ReadFtp.log";
+			}
+		}
+
+		public new static ReadFtpLog Manager
+		{
+			get
+			{
+				return (ReadFtpLog)Logger.Manager[typeof(ReadFtpLog)];
+			}
+		}
+	}
+}
